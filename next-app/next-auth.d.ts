ef2cc2553daaa435ef9def58e26c0acd8b000397
MSK1206 <phantom7216@yahoo.co.jp>
2024-03-31 23:00:09 +0900

    Add: nextauth user definition.

---
+++ next-app/next-auth.d.ts
@@ -0,0 +1,18 @@
+import NextAuth, { DefaultSession } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      role: string;
+    } & DefaultSession['user'];
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id: string;
+    role: string;
+  }
+}
