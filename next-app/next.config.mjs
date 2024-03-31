d97bb646ed55fd6ddac677de098ec85a510bc7c7
MSK1206 <phantom7216@yahoo.co.jp>
2024-03-31 23:00:49 +0900

    Add: provider image host.

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      // Google User Account Image.
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      // GitHub User Account Image.
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
